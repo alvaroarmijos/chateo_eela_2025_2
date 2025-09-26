4126dc87b13329b7c80426c80ff9dff697eab62c
Alvaro Armijos <alvaro.armijoss@gmail.com>
2025-09-25 20:42:13 -0500

    Implement Onboarding UI.

--- lib/onboarding/onboarding_page.dart
+++ lib/onboarding/onboarding_page.dart
@@ -7,8 +7,96 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(children: [Image.asset('assets/icons/icon_app.png')]),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset('assets/icons/icon_app.png'),
+                  Text(
+                    'Connect friends easily & quickly',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 68,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    'Our chat app is the perfect way to stay connected with friends and family.',
+                    style: TextStyle(color: Color(0xffB9C1BE), fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 20,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          print('Login with Facebook');
+                        },
+                        child: Image.asset('assets/icons/icon_facebook.png'),
+                      ),
+                      Image.asset('assets/icons/icon_google.png'),
+                      Image.asset('assets/icons/icon_apple.png'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(child: Divider(color: Colors.grey)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          'OR',
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                      ),
+                      Expanded(child: Divider(color: Colors.grey)),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text('Sign up withn mail'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Existing account? ',
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                        children: [
+                          TextSpan(
+                            text: 'Log in',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
